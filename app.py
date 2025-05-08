86bf5d2f5d583cd2d9ada4a3e1c8b678319530b9
Eduardo-J-Morales <eduardojosegonzalezmorales41@gmail.com>
2025-05-08 11:09:36 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import json
 import nltk
-from nltk.tolkenize import word_tokenize
+from  nltk.tokenize import word_tokenize
 
 nltk.download('punkt')
 
 app = Flask(__name__)
 
-with
+with open('knowledge.json') as f:
+    knowledge = json.load(f)
+
+def recognize_intent(user_input):
+    tokens = word_tokenize(user_input.lower())
+    for item in knowledge:
+        for pattern in item['patterns']:
