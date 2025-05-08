67c8e97297cfeff309db938472c4b06180750b2f
Eduardo-J-Morales <eduardojosegonzalezmorales41@gmail.com>
2025-05-08 09:46:30 -0400

    Deleted app.py, config.py, models.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'mySecretKey')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/authdb')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
